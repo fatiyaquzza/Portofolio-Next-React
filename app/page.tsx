ec6f80258425eefbd045ca8c06a416659c330b55
Fatiya Quzza <fatiya_qu@mhs.unsyiah.ac.id>
2025-07-29 17:53:17 +0700

    add bg[

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 import Lanyard from "./components/Lanyard/Lanyard";
-import {
-  Navbar,
-  NavBody,
-  NavItems,
-  MobileNav,
-  NavbarLogo,
-  NavbarButton,
-  MobileNavHeader,
-  MobileNavToggle,
-  MobileNavMenu,
-} from "./components/ui/resizeable-navbar";
+// import {
+//   Navbar,
+//   NavBody,
+//   NavItems,
+//   MobileNav,
+//   NavbarLogo,
+//   NavbarButton,
+//   MobileNavHeader,
+//   MobileNavToggle,
+//   MobileNavMenu,
+// } from "./components/ui/resizeable-navbar";
 import { useState } from "react";
+import DarkVeil from "./components/DarkVeil/DarkVeil";
 
 export default function Home() {
   const navItems = [
@@ -37,9 +38,8 @@ export default function Home() {
 
   return (
     <div className="min-h-[200vh] overflow-x-hidden bg-[#131320] scroll-smooth">
-      <div className="mt-4 border-1 border-[#ffffff5c] mx-16 py-2">
+      {/* <div className="mt-4 border-1 border-[#ffffff5c] mx-16 py-2">
         <Navbar>
-          {/* Desktop Navigation */}
           <NavBody>
             <NavbarLogo />
             <NavItems items={navItems} />
@@ -48,7 +48,6 @@ export default function Home() {
             </div>
           </NavBody>
 
-          {/* Mobile Navigation */}
           <MobileNav>
             <MobileNavHeader>
               <NavbarLogo />
@@ -91,13 +90,58 @@ export default function Home() {
             </MobileNavMenu>
           </MobileNav>
         </Navbar>
+      </div> */}
+
+      <div className="fixed top-6 left-1/2 -translate-x-1/2 z-[999] w-full px-4">
+        <div className="backdrop-blur-lg bg-white/5 border border-white/10 px-8 py-3 rounded-full flex items-center justify-between max-w-screen-lg mx-auto">
+          {/* Left: Logo /} */}
+          <span className="text-white font-semibold flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5 text-[#6184DC]"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M12 6v6l4 2"
+              />
+            </svg>
+            FATIYA.
+          </span>
+
+          {/* {/ Right: Menu */}
+          <div className="flex gap-6 text-white text-sm font-medium">
+            <a href="#home" className="hover:text-[#6184DC] transition">
+              Home
+            </a>
+            <a href="#About" className="hover:text-[#6184DC] transition">
+              About
+            </a>
+            <a href="#project" className="hover:text-[#6184DC] transition">
+              Project
+            </a>
+            <a href="#contact" className="hover:text-[#6184DC] transition">
+              Contact
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="mx-auto h-screen bg-[#131320]">
+
+      <div className="mx-auto h-screen bg-[#131320] " id="home">
         <div className="grid grid-cols-12 bg-[#131320]">
-          <div className="col-span-4">
+          <DarkVeil />
+          <div className="col-span-5">
             <Lanyard position={[0, 0, 14]} gravity={[0, -90, 0]} />
           </div>
-          <div className="col-span-6"></div>
+          <div className="col-span-4 flex justify-center items-center">
+            <h1 className="text-semibold text-white mx-auto text-5xl">
+              Hi, i am Fatiya Quzza
+            </h1>
+          </div>
         </div>
       </div>
 
